63a40a669e40cd9a80b67971d31beccc97e61a8f
Benny Yen <benny123tw@gmail.com>
2025-02-06 10:26:45 +0000

    feat: add tdd-demo layout

---
+++ layouts/tdd-demo.vue
@@ -0,0 +1,26 @@
+<script setup lang="ts">
+const props = defineProps({
+  class: {
+    type: String,
+  },
+  layoutClass: {
+    type: String,
+  },
+  testResult: {
+    type: Boolean,
+    required: true,
+  }
+})
+</script>
+
+<template>
+  <slot name="header" />
+  <div class="slidev-layout two-columns w-full h-full grid grid-cols-2 items-start gap-4" :class="props.layoutClass">
+    <div class="col-left" :class="props.class">
+      <slot name="left" />
+    </div>
+    <div class="col-right" :class="props.class">
+      <slot name="right" />
+    </div>
+  </div>
+</template>
